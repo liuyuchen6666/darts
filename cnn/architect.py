123f0fae24fcc341fbbbb18eb1a4ebdfa23b19a2
liuyuchen6666 <55131290+liuyuchen6666@users.noreply.github.com>
2020-09-25 15:08:17 +0800

    Update architect.py

--- cnn/architect.py
+++ cnn/architect.py
@@ -4,27 +4,33 @@ import torch.nn as nn
 from torch.autograd import Variable
 
 
-def _concat(xs):
+def _concat(xs):#将向量全部展开为一维
   return torch.cat([x.view(-1) for x in xs])
 
 
 class Architect(object):
-
+#计算alpha的梯度
   def __init__(self, model, args):
-    self.network_momentum = args.momentum
-    self.network_weight_decay = args.weight_decay
-    self.model = model
+    self.network_momentum = args.momentum #设置动量
+    self.network_weight_decay = args.weight_decay#设置权重衰减正则化
+    self.model = model 
+    #设置优化器训练alpha
     self.optimizer = torch.optim.Adam(self.model.arch_parameters(),
         lr=args.arch_learning_rate, betas=(0.5, 0.999), weight_decay=args.arch_weight_decay)
 
   def _compute_unrolled_model(self, input, target, eta, network_optimizer):
-    loss = self.model._loss(input, target)
-    theta = _concat(self.model.parameters()).data
-    try:
+    loss = self.model._loss(input, target)#计算前向误差
+    theta = _concat(self.model.parameters()).data #将权重展开为向量形式
+    try:#上一步缓存的动量乘以momentum系数
       moment = _concat(network_optimizer.state[v]['momentum_buffer'] for v in self.model.parameters()).mul_(self.network_momentum)
     except:
       moment = torch.zeros_like(theta)
+    #求得动量
+    #moment = mu * v_{t} 
+    #dw = g_{t+1} + weight_decay * w 
+    #v_{t+1} = moment + dw
     dtheta = _concat(torch.autograd.grad(loss, self.model.parameters())).data + self.network_weight_decay*theta
+    #self._construct_model_from_theta ?
     unrolled_model = self._construct_model_from_theta(theta.sub(eta, moment+dtheta))
     return unrolled_model
 
@@ -32,7 +38,7 @@ class Architect(object):
     self.optimizer.zero_grad()
     if unrolled:
         self._backward_step_unrolled(input_train, target_train, input_valid, target_valid, eta, network_optimizer)
-    else:
+    else:#如果不展开即相当于first order近似，直接求val loss进行更新
         self._backward_step(input_valid, target_valid)
     self.optimizer.step()
 
@@ -41,25 +47,29 @@ class Architect(object):
     loss.backward()
 
   def _backward_step_unrolled(self, input_train, target_train, input_valid, target_valid, eta, network_optimizer):
+    #首先更新一次权重系数w，得到w'
     unrolled_model = self._compute_unrolled_model(input_train, target_train, eta, network_optimizer)
-    unrolled_loss = unrolled_model._loss(input_valid, target_valid)
-
-    unrolled_loss.backward()
+    unrolled_loss = unrolled_model._loss(input_valid, target_valid)#L_val(w')
+    #更新之后计算误差
+    unrolled_loss.backward()#计算更新alpha的值
     dalpha = [v.grad for v in unrolled_model.arch_parameters()]
+    #计算关于alpha的梯度
     vector = [v.grad.data for v in unrolled_model.parameters()]
+    #计算关于权重系数的梯度
     implicit_grads = self._hessian_vector_product(vector, input_train, target_train)
-
+    #计算最后的梯度 gradient = dalpha - eta*implicit_grads
     for g, ig in zip(dalpha, implicit_grads):
       g.data.sub_(eta, ig.data)
 
     for v, g in zip(self.model.arch_parameters(), dalpha):
       if v.grad is None:
-        v.grad = Variable(g.data)
+        v.grad = Variable(g.data) #将计算得到的梯度值传给模型参数更新
       else:
         v.grad.data.copy_(g.data)
 
   def _construct_model_from_theta(self, theta):
-    model_new = self.model.new()
+    #根据权重系数建立新模型
+    model_new = self.model.new() #创立新模型
     model_dict = self.model.state_dict()
 
     params, offset = {}, 0
@@ -74,9 +84,9 @@ class Architect(object):
     return model_new.cuda()
 
   def _hessian_vector_product(self, vector, input, target, r=1e-2):
-    R = r / _concat(vector).norm()
+    R = r / _concat(vector).norm()#.norm()?eps
     for p, v in zip(self.model.parameters(), vector):
-      p.data.add_(R, v)
+      p.data.add_(R, v) #w = w+eps*dw
     loss = self.model._loss(input, target)
     grads_p = torch.autograd.grad(loss, self.model.arch_parameters())
 
@@ -87,6 +97,6 @@ class Architect(object):
 
     for p, v in zip(self.model.parameters(), vector):
       p.data.add_(R, v)
-
+    #求得海森矩阵
     return [(x-y).div_(2*R) for x, y in zip(grads_p, grads_n)]
 
